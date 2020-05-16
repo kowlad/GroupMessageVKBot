ca3ec289e4c6b630464d18dfa2b41b4f3b7f8ee8
Wlad Kowalew <wladickowalew@gmail.com>
2020-05-16 15:45:18 +0300

    First commit

---
+++ Project/config.py
@@ -0,0 +1,5 @@
+def getId():
+    return "YOUR_GROUP_ID";
+
+def getToken():
+    return "YOUR_TOKEN";
---
+++ Project/main.py
@@ -0,0 +1,10 @@
+import vk_api
+from Project.config import getId, getToken
+
+
+def main():
+    print("Скоро здесь будет бот")
+
+
+if __name__ == '__main__':
+    main()
